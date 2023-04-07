8430f76479e74b2faf04dea063a693b0f93fda29
Rob Connolly <rconnolly@macmillan.org.uk>
2023-04-07 12:25:21 +0100

    add links to blog pages

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -35,6 +36,8 @@ export default function Home({ allPostsData }) {
               {id}
               <br />
               {date}
+              <br />
+              <Link href={`posts/${id}`}>{title}</Link>
             </li>
           ))}
         </ul>
